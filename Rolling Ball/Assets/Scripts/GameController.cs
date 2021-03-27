1a7b21d68a1215aae7acc9f8997fbee60ed89b6a
gabekao <38321687+gabekao@users.noreply.github.com>
2021-03-26 20:55:30 -0500

    UPDATE
    * Rollback Stage1 Import
    * Organize scene
    * Add R to restart

---
+++ Rolling Ball/Assets/Scripts/GameController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("r")){
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            //Debug.Log("R");
+        }    
+    }
+
+    // private void OnTriggerEnter(Collider other){
+    //     if (transform.IsChildOf("Checkpoints")){
+    //         Debug.Log("Checkpoint!");
+    //     }
+    // }
+}
