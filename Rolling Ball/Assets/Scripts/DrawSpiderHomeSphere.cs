d04150dabf38d1e8c6aaa2fd38be7ae829d4e023
Paul Lapsley <51719661+no5544554@users.noreply.github.com>
2021-05-02 01:14:27 -0500

    Additional Spider Scripts

---
+++ Rolling Ball/Assets/Scripts/DrawSpiderHomeSphere.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawSpiderHomeSphere : MonoBehaviour
+{
+    // this is to draw the radius of the sphere in the scene view for convenience
+    [SerializeField] private SpiderRobotScript srs;
+    private float gizmoRadius;
+
+    private void FixedUpdate()
+    {
+        if (srs !=null)
+            gizmoRadius = srs.PatrolRadius;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (srs != null)
+            Gizmos.color = new Color(0xFF, 0xFF, 0x88);
+            Gizmos.DrawWireSphere(transform.position, gizmoRadius);
+    }
+}
